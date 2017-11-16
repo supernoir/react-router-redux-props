66404faf8e27be678e5fd64c1471759c2239ddde
Conny Kawohl <conny.kawohl@useeds.de>
2017-11-16 15:58:49 +0100

    adds simple routes

--- js/components/App.js
+++ js/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Router, Route } from 'react-router-dom'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1>React Router Redux Props</h1>
+        <Router>
+          <Route path="/" component={Home} />
+          <Route paht="/about" component={About} />
+          <Route path="/fairygodmothers/:name" component={Pumpkins} />
+        </Router>
       </div>
     )
   }
